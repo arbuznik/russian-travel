d0fe973966e03e214239bc3ca4754fc7bfe936c5
Arbuznik <arbyzov.nikita@yandex.ru>
2021-09-28 01:55:51 +0300

    refactor: make better animation

--- blocks/lead/__travel-route/lead__travel-route.js
+++ blocks/lead/__travel-route/lead__travel-route.js
@@ -1,66 +1,45 @@
-let routePath = document.querySelector('.lead__travel-route-path');
-let routePathStyles = getComputedStyle(routePath);
+const routePath = document.querySelector('.lead__travel-route-path');
+const routePathStyles = getComputedStyle(routePath);
 const timer = ms => new Promise(res => setTimeout(res, ms));
 
-let novgorod = document.querySelector('.lead__travel-route-city_name_novgorod');
-let pskov = document.querySelector('.lead__travel-route-city_name_pskov');
-let tver = document.querySelector('.lead__travel-route-city_name_tver');
-let yaroslavl = document.querySelector('.lead__travel-route-city_name_yaroslavl');
-let manturovo = document.querySelector('.lead__travel-route-city_name_manturovo');
-let kotelnich = document.querySelector('.lead__travel-route-city_name_kotelnich');
-let glazov = document.querySelector('.lead__travel-route-city_name_glazov');
-let perm = document.querySelector('.lead__travel-route-city_name_perm');
-let tymen = document.querySelector('.lead__travel-route-city_name_tymen');
-let omsk = document.querySelector('.lead__travel-route-city_name_omsk');
-let krasnoyarsk = document.querySelector('.lead__travel-route-city_name_krasnoyarsk');
-let ulanUde = document.querySelector('.lead__travel-route-city_name_ulan-ude');
+const OFFSETS = [
+  { city: 'ulan-ude', offset: 25 },
+  { city: 'krasnoyarsk', offset: 110 },
+  { city: 'omsk', offset: 320 },
+  { city: 'tymen', offset: 440 },
+  { city: 'perm', offset: 500 },
+  { city: 'glazov', offset: 590 },
+  { city: 'kotelnich', offset: 780 },
+  { city: 'manturovo', offset: 880 },
+  { city: 'yaroslavl', offset: 1010 },
+  { city: 'tver', offset: 1100 },
+  { city: 'novgorod', offset: 1280 }
+];
+
+const cityElements = OFFSETS.map((item) => {
+  let cityElement = document.querySelector(`.lead__travel-route-city_name_${item.city}`);
+  return cityElement;
+})
 
 function getCurrentDashOffset() {
   let offset = routePathStyles.strokeDashoffset.slice(0, -2);
   return Math.floor(offset);
 }
 
-async function trackOffset() { 
-  while (getCurrentDashOffset() > 0 ) {
-    switch (true)  {
-      case (getCurrentDashOffset() < 15):
-        ulanUde.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 100):
-        krasnoyarsk.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 310):
-        omsk.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 430):
-        tymen.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 490):
-        perm.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 580):
-        glazov.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 770):
-        kotelnich.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 870):
-        manturovo.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 1000):
-        yaroslavl.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 1090):
-        tver.classList.add('lead__travel-route-city_is-shown');
-        break;
-      case (getCurrentDashOffset() < 1270):
-        novgorod.classList.add('lead__travel-route-city_is-shown');
-        break;
-      default:
-        break;
+async function trackOffset() {
+  let currentOffset = getCurrentDashOffset();
+  i = 1;
+  while (currentOffset > 0) {
+    let lastElement = OFFSETS[OFFSETS.length - i];
+
+    if (currentOffset < lastElement.offset) {
+      cityElements[OFFSETS.length - i].classList.add('lead__travel-route-city_is-shown');
+      i++;
     }
+
     await timer(50);
+    currentOffset = getCurrentDashOffset();
   }
-}
+}     
 
 trackOffset();
